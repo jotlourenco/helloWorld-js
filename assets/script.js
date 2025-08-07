4e37fe6f458b588a63e9acc44673ca3badaf6891
João <jotlourenco@outlook.com>
2025-08-06 23:02:14 -0300

    aula 01

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+let idade;
+let salario;
+let nome = prompt("Digite seu nome:");
+
+
+
+
+console.log("Olá, mundo!");
+
+console.log("Olá, " + nome);
+
+alert("Olá, " + nome);
+
+document.write("Olá, " + nome);
+
